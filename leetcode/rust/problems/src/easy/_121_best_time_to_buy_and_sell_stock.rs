43780f527b76304c0cf4b0925a616c599e3b9a5d
Mathias <mawemw5@gmail.com>
2024-09-16 19:30:24 +0200

    Improved solution of Problem 121 - Best Time to Buy and Sell Stock

--- leetcode/rust/problems/src/easy/_121_best_time_to_buy_and_sell_stock.rs
+++ leetcode/rust/problems/src/easy/_121_best_time_to_buy_and_sell_stock.rs
@@ -3,7 +3,30 @@ struct Solution;
 
 #[allow(dead_code)]
 impl Solution {
+
     pub fn max_profit (prices: Vec<i32>) -> i32 {
+        if prices.len() < 2 {
+            return 0;
+        }
+
+        let mut buy = prices[0];
+        let mut sell = 0;
+
+        for &price in &prices[1..] {
+            if price < buy {
+                buy = price;
+            } else if price - buy > sell {
+                sell = price - buy;
+            }
+        }
+
+        sell
+    }
+
+    /**
+     * First solution but can be done better
+     */
+    pub fn max_profit_b (prices: Vec<i32>) -> i32 {
         if prices.len() == 0 {
             return 0;
         }
@@ -39,4 +62,4 @@ pub fn test_max_profit () {
     assert_eq!(Solution::max_profit(a), 5);
     assert_eq!(Solution::max_profit(b), 0);
     assert_eq!(Solution::max_profit(c), 19);
-}   
+}
